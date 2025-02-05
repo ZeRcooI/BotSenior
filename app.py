8001594d66d93bc42c1056a327f92e6e43662c67
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-02-05 23:18:50 +0300

    Добавлены ещё команды для обращения к боту

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
+from handlers.user_private import user_private_router
 import asyncio
 import os
-from aiogram import Bot, Dispatcher, types
+
+from aiogram import Dispatcher, types, Bot
 from aiogram.filters import CommandStart
 
 from dotenv import find_dotenv, load_dotenv
-
 load_dotenv(find_dotenv())
+
 
 ALLOWED_UPDATES = ['message, edited_message']
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Привет, я тебе помогу со всеми вопросами!')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+dp.include_router(user_private_router)
 
 
 async def main():
