4f4e92823f79385a54e04436838faf5bbf58341f
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-02-08 00:13:50 +0300

    Обновлена логика добавления кнопок как конструктор, изменен скрипт в личке с ботом

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -4,7 +4,7 @@ from aiogram.filters import CommandStart, Command, or_f
 from aiogram.utils.formatting import as_list, as_marked_section, Bold
 
 from filters.chat_types import ChatTypeFilter
-from kbds import reply
+from kbds.reply import get_keyboard
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(['private']))
@@ -12,65 +12,72 @@ user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer("Рождён, чтобы ебашить!", reply_markup=reply.start_kb3.as_markup(
-        resize_keyboard=True,
-        input_field_placeholder='Что вас интересует?'
-    ))
+    await message.answer(
+        "Рождён, чтобы ебашить!",
+        reply_markup=get_keyboard(
+            "Меню",
+            "Про бота",
+            "Варианты оплаты",
+            "Варианты доставки",
+            placeholder="Что вас интересует?",
+            sizes=(2, 2)
+        ))
 
 
 @user_private_router.message(or_f(Command('menu'), (F.text.lower() == 'меню')))
 async def menu_cmd(message: types.Message):
-    await message.answer("Вот меню: ", reply_markup=reply.del_kbd)
+    await message.answer("Вот меню: ")
 
 
-@user_private_router.message(F.text.lower() == "о нас")
+@user_private_router.message(F.text.lower() == "Про бота")
 @user_private_router.message(Command('about'))
 async def about_cmd(message: types.Message):
-    await message.answer("О нас: ")
+    await message.answer("Про бота: ")
 
 
-@user_private_router.message(F.text.lower() == "варианты оплаты")
-@user_private_router.message(Command('payment'))
+@user_private_router.message(F.text.lower() == "Варианты оплаты")
+@user_private_router.message(Command("payment"))
 async def payment_cmd(message: types.Message):
     text = as_marked_section(
-        Bold('Варианты оплаты: '),
-        'Картой в боте',
-        'При получении карта/кеш',
-        'В заведении',
-        marker='✅ '
+        Bold("Варианты оплаты:"),
+        "Картой в боте",
+        "При получении карта/кеш",
+        "В заведении",
+        marker="✅ ",
     )
     await message.answer(text.as_html())
 
 
-@user_private_router.message((F.text.lower().contains('доставк')) | (F.text.lower() == "варианты доставки товара"))
-@user_private_router.message(Command('shipping'))
-async def shipping_cmd(message: types.Message):
+@user_private_router.message(
+    (F.text.lower().contains("доставк")) | (F.text.lower() == "Варианты доставки"))
+@user_private_router.message(Command("shipping"))
+async def menu_cmd(message: types.Message):
     text = as_list(
         as_marked_section(
-            Bold('Варианты доставки заказа: '),
-            'Курьер',
-            'Самовынос (сейчас прибегу заберу)',
-            'Покушаю у вас (сейчас прибегу)',
-            marker='✅ '
+            Bold("Варианты доставки/заказа:"),
+            "Курьер",
+            "Самовынос (сейчас прибегу заберу)",
+            "Покушаю у Вас (сейчас прибегу)",
+            marker="✅ ",
         ),
         as_marked_section(
-            Bold('Нельзя: '),
-            'Почта',
-            'Голуби',
-            marker='❌ '
+            Bold("Нельзя:"),
+            "Почта",
+            "Голуби",
+            marker="❌ "
         ),
-        sep='\n-----------------------------\n'
+        sep="\n----------------------\n",
     )
     await message.answer(text.as_html())
 
 
-@user_private_router.message(F.contact)
-async def get_contact(message: types.Message):
-    await message.answer(f"Номер получен")
-    await message.answer(str(message.contact))
+# @user_private_router.message(F.contact)
+# async def get_contact(message: types.Message):
+#     await message.answer(f"Номер получен")
+#     await message.answer(str(message.contact))
 
 
-@user_private_router.message(F.location)
-async def get_location(message: types.Message):
-    await message.answer(f"Локация получена")
-    await message.answer(str(message.location))
+# @user_private_router.message(F.location)
+# async def get_location(message: types.Message):
+#     await message.answer(f"Локация получена")
+#     await message.answer(str(message.location))
