3257936e6377655d67c6c230e77ba795bf9c026b
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-02-09 03:59:12 +0300

    подготовлена база данных

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from common.bot_cmds_list import private
-from aiogram.client.default import DefaultBotProperties
 from handlers.admin_private import admin_router
 from handlers.user_group import user_group_router
 from handlers.user_private import user_private_router
-import asyncio
-import os
-
+from common.bot_cmds_list import private
+from aiogram.client.default import DefaultBotProperties
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
-
+from database.engine import create_db, drop_db
+import asyncio
+import os
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
@@ -26,7 +25,22 @@ dp.include_router(user_group_router)
 dp.include_router(admin_router)
 
 
+async def on_startup(bot):
+    run_param = False
+    if run_param:
+        await drop_db()
+
+    await create_db()
+
+
+async def on_shutdown(bot):
+    print('бот лёг')
+
+
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
